79084bb99a8b0e90432faa6163159627c8d64e57
ganxunzou <ganxunzou@163.com>
2018-08-30 17:54:24 +0800

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import React, { Component, PropTypes } from 'react';
-import Header from './header';
-import Body from './body';
-import Footer from './footer';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Header from './views/header';
+import Body from './views/body';
+import Footer from './views/footer';
 import style from './App.css';
 
 class App extends Component {
   render() {
     return (
-            <div className={style.main}>
-                <Header />
-                <Body />
-                <Footer />
-            </div>
+			<div className={style.main}>
+				<Router>
+					<div>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>
+							<li>
+								<Link to="/topics">Topics</Link>
+							</li>
+						</ul>
+						<hr />
+
+						<Route exact path="/" component={Header} />
+						<Route path="/about" component={Body} />
+						<Route path="/topics" component={Footer} />
+					</div>
+				</Router>
+			</div>
     );
   }
 }
 
-App.propTypes = {
-
-};
+App.propTypes = {};
 
 export default App;
